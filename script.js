a57efacd753d2917d5b1b530f1a869a59fb2456f
Isis Alexander <goddess.isis.alexander@gmail.com>
2022-04-03 23:30:19 -0700

    "fixed bits here and there with my tutor to get the code to work"

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function generatePassword(){
+function generatePassword() {
   //TODO: your code goes here
-<<<<<<< HEAD
-=======
   return "password"
->>>>>>> e3ffeda00fd023743152fb8618f6305546f34605
 }
-
-
 
 // TO DO LIST
 // create a rando password with letters, numbers, upper/lowercase and special characters
@@ -22,54 +17,53 @@ var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 var numbers = ("0123456789")
 var specialCharacters = ("!@#$%^&*_-+=")
 var userChoice = ("")
-var userAnswer =("")
+var userAnswer = ("")
 
 // the stuff the homework provided
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
-  var password = generatePassword();
+  var password = getUseranswer();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
 
-  // getting the preferred length of the password and putting said input into the rest of the equation but this part wasn't allowed in the function???
+function getUseranswer() {
+  // getting the preferred length of the password from user
   userAnswer = window.prompt("how long do you want the password to be?");
-  return userAnswer
+  var passwordText = ""
 
-  function getUseranswer(){
-  if (userAnswer <= 8|| userAnswer >128){
-    window.alert ("password has to be a minimum of 8 characters or to the maximum of 128 characters.")
+  if (userAnswer <= 8 || userAnswer > 128) {
+    window.alert("password has to be a minimum of 8 characters or to the maximum of 128 characters.")
     return "error"
   }
 
-  let;
-  // window question to confirm if they want item and then randomizing said item for password contents
-    if (window.confirm("do you want uppercase letters?") == true) {
-    userChoice = userChoice + upperCase}     
-    for (var i = 0; i < uppercaseValue; i++){
-     upperCase = (Math.floor(Math.random()*upperCase.length));
-}
-    if (window.confirm("do you want lowercase letters?") == true) {
-    userChoice = userChoice + lowerCase}
-     for (var i = 0; i < lowercaseValue; i++){
-      lowerCase = (Math.floor(Math.random()*lowerCase.length));
-}
-    if (window.confirm("do you want numbers?") == true){
-    userChoice = userChoice + numbers}
-    for (var i = 0; i < numberValue; i++){
-      numbers = (Math.floor(Math.random()*numbers.length));
-}
-    if (window.confirm("do you want special characters?") == true) {
-  userChoice = userChoice + specialCharacters}
-      for (var i = 0; i < specialValue; i++){
-      specialCharacters = (Math.floor(Math.random()*specialCharacters.length));
-      }
+  // window question to confirm if they want in their password
+  if (window.confirm("do you want uppercase letters?") == true) {
+    userChoice = userChoice + upperCase
+  }
+  
+  if (window.confirm("do you want lowercase letters?") == true) {
+    userChoice = userChoice + lowerCase
+  }
 
-      // but somehow make it as long as the userAnswer????
-  (passwordText = specialValue + numberValue + lowerCase + uppercaseValue)  
+  if (window.confirm("do you want numbers?") == true) {
+    userChoice = userChoice + numbers
+  }
+ 
+  if (window.confirm("do you want special characters?") == true) {
+    userChoice = userChoice + specialCharacters
+  }
+  
+  // add everything together that the user chose, make the desired length and randomize it
+  for (var i = 0; i < userAnswer; i++) {
+    var index = (Math.floor(Math.random() * userChoice.length));
+    passwordText += userChoice[index]
+  }
+
+  return passwordText
 }
-return password
 
 
 
